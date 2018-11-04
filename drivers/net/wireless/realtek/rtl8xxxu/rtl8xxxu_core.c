97a8a5b3c157e593637aed32800d0a14eaa9e9d6
rafirafi <rafirafi.at@gmail.com>
2018-11-04 22:56:11 +0100

    rtl8xxxu : remove gen1 parts from gen2 update rate mask.
    Signed-off-by: Rafael Gandolfi <rafirafi.at@gmail.com>

--- drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu_core.c
+++ drivers/net/wireless/realtek/rtl8xxxu/rtl8xxxu_core.c
@@ -4328,7 +4328,6 @@ void rtl8xxxu_gen2_update_rate_mask(struct rtl8xxxu_priv *priv,
 				    u32 ramask, int sgi)
 {
 	struct h2c_cmd h2c;
-	u8 bw = 0;
 
 	memset(&h2c, 0, sizeof(struct h2c_cmd));
 
@@ -4338,15 +4337,14 @@ void rtl8xxxu_gen2_update_rate_mask(struct rtl8xxxu_priv *priv,
 	h2c.b_macid_cfg.ramask2 = (ramask >> 16) & 0xff;
 	h2c.b_macid_cfg.ramask3 = (ramask >> 24) & 0xff;
 
-	h2c.ramask.arg = 0x80;
 	h2c.b_macid_cfg.data1 = 0;
 	if (sgi)
 		h2c.b_macid_cfg.data1 |= BIT(7);
 
-	h2c.b_macid_cfg.data2 = bw;
+	h2c.b_macid_cfg.data2 = 0;
 
-	dev_dbg(&priv->udev->dev, "%s: rate mask %08x, arg %02x, size %zi\n",
-		__func__, ramask, h2c.ramask.arg, sizeof(h2c.b_macid_cfg));
+	dev_dbg(&priv->udev->dev, "%s: rate mask %08x, sgi %02x, size %zi\n",
+		__func__, ramask, sgi, sizeof(h2c.b_macid_cfg));
 	rtl8xxxu_gen2_h2c_cmd(priv, &h2c, sizeof(h2c.b_macid_cfg));
 }
 
